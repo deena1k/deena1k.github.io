2b89cc00ff1d3b234b7a95d6518b0c9d426245a6
unknown <adeena@mit.edu>
2024-09-28 00:51:59 -0400

    first project

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 var linkedln = document.getElementById("link");
 linkedln.addEventListener("click",redirect)
 
+
+
 function redirect()
 {
-    window.location.href = "https://www.linkedin.com/in/adeena-khan/"
+    window.open("https://www.linkedin.com/in/adeena-khan/")
 }
